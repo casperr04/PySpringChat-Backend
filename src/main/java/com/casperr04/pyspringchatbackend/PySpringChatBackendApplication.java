cc60d67d3a36633ae7da3fd5777a8df8846606a3
Kacper <kacperjaroslawurbanczyk@gmail.com>
2023-04-24 17:37:45 +0200

    init

---
+++ src/main/java/com/casperr04/pyspringchatbackend/PySpringChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.casperr04.pyspringchatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PySpringChatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PySpringChatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/casperr04/pyspringchatbackend/PySpringChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casperr04.pyspringchatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PySpringChatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
