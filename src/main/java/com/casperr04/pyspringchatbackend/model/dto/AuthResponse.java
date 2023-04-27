0248da1babcf254da273f6d317ddf7159e5db727
Kacper <kacperjaroslawurbanczyk@gmail.com>
2023-04-27 20:00:08 +0200

    AuthResponse fields are now more explicit on what the fields contain

--- src/main/java/com/casperr04/pyspringchatbackend/model/dto/AuthResponse.java
+++ src/main/java/com/casperr04/pyspringchatbackend/model/dto/AuthResponse.java
@@ -1,5 +1,9 @@
 package com.casperr04.pyspringchatbackend.model.dto;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,9 +12,19 @@ import java.time.Instant;
 @Data
 @Builder
 public class AuthResponse {
+    @NotNull
     private Long id;
+
+    @NotBlank
     private String username;
-    private Instant dateOfCreation;
+
+    @PastOrPresent
+    private Instant accountDateOfCreation;
+
+    @NotBlank
     private String token;
+
+    @FutureOrPresent
+    private Instant tokenExpirationDate;
 }
 
