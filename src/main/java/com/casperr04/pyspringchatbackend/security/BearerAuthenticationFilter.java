2b4967dc59e4ca9fbbb77d6bf30303dfad2d521b
Kacper <kacperjaroslawurbanczyk@gmail.com>
2023-04-30 19:03:13 +0200

    fix: Changed BearerAuthenticationFilter AllArgsConstructor to RequiredArgsConstructor
    For some reason the tests wouldn't work beforehand, this fixed it.

--- src/main/java/com/casperr04/pyspringchatbackend/security/BearerAuthenticationFilter.java
+++ src/main/java/com/casperr04/pyspringchatbackend/security/BearerAuthenticationFilter.java
@@ -6,7 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,7 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Component
 public class BearerAuthenticationFilter extends OncePerRequestFilter {
     private AuthTokenRepository authTokenRepository;
