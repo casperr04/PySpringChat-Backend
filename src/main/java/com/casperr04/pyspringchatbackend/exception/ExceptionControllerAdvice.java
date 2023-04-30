99b88120b4fb6259245af3ad38dc1fc8a763e6db
Kacper <kacperjaroslawurbanczyk@gmail.com>
2023-04-30 19:05:03 +0200

    feat: Exception Handler for InvalidCredentialsException

--- src/main/java/com/casperr04/pyspringchatbackend/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/casperr04/pyspringchatbackend/exception/ExceptionControllerAdvice.java
@@ -3,6 +3,7 @@ package com.casperr04.pyspringchatbackend.exception;
 import com.casperr04.pyspringchatbackend.model.dto.ExceptionResponseModel;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -30,6 +31,12 @@ public class ExceptionControllerAdvice {
     public ResponseEntity<ExceptionResponseModel> handleConstraintViolationException(ConstraintViolationException e){
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponseModel("At least one of the request fields does not fit constraints", Instant.now()));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ExceptionResponseModel> handleInvalidCredentialsException(AuthenticationException e){
+        return ResponseEntity.badRequest()
+                .body(new ExceptionResponseModel("Invalid username or password", Instant.now()));
     }
 
 }
