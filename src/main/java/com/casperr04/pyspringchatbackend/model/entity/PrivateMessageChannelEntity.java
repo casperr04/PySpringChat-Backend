7eed742d41edd9f61e3475dc50c5e5770578cf8e
casperr04 <kacperjaroslawurbanczyk@gmail.com>
2023-10-02 11:31:34 +0200

    fix: removed unused manytomany table from PrivateMessageChannelEntity

--- src/main/java/com/casperr04/pyspringchatbackend/model/entity/PrivateMessageChannelEntity.java
+++ src/main/java/com/casperr04/pyspringchatbackend/model/entity/PrivateMessageChannelEntity.java
@@ -1,11 +1,11 @@
 package com.casperr04.pyspringchatbackend.model.entity;
 
 import com.casperr04.pyspringchatbackend.model.entity.superclasses.ChannelEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-
-import java.util.Set;
 
 @Entity
 @Table(name = "private_message_channel")
@@ -24,13 +24,4 @@ public class PrivateMessageChannelEntity extends ChannelEntity {
         @ManyToOne
         @NotNull
         private UserEntity recipient;
-
-        @ManyToMany
-        @JoinTable(
-                name = "private_channel_messages",
-                joinColumns = @JoinColumn(name = "channel_id"),
-                inverseJoinColumns = @JoinColumn(name = "message_id")
-        )
-        @ToString.Exclude
-        private Set<PrivateMessageEntity> messages;
     }
